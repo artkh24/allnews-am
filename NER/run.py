6037a6361c7c90cfd38a06d22f8b9518ade158b2
Artur Khondkaryan <artkh24@gmail.com>
2020-01-22 17:39:23 +0400

    code correction

--- NER/run.py
+++ NER/run.py
@@ -12,8 +12,8 @@ Options:
     --model-root=<file>               the path to the directory for model files
                                       [default: ./models]
     --batch-size=<int>                batch size [default: 32]
-    --max-epoch=<int>                 max epoch [default: 30]
-    --max-len=<int>                   sentence max size [default: 75]
+    --max-epoch=<int>                 max epoch [default: 1]
+    --max-len=<int>                   sentence max size [default: 128]
     --lr=<float>                      learning rate [default: 3e-5]
     --train-test-split=<float>        train test split [default: 0.1]
     --full-finetuning                 use full finetuning
@@ -34,7 +34,24 @@ import sys
 from docopt import docopt
 import sentence
 from tqdm import trange
+from BERT.util_ner import convert_examples_to_features, get_labels, read_examples_from_file
 
+
+def loadData(tokenizer,datapath,mode,max_length,labels):
+    examples = read_examples_from_file(datapath, mode=mode)
+    features = convert_examples_to_features(
+        examples,
+        labels,
+        max_length,
+        tokenizer,
+    )
+    all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
+    all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
+    all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
+    all_label_ids = torch.tensor([f.label_ids for f in features], dtype=torch.long)
+
+    dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
+    return dataset
 
 
 def flat_accuracy(preds, labels):
@@ -47,7 +64,6 @@ def train(args: Dict):
     bs = int(args['--batch-size'])
     model_root = args['--model-root'] if args['--model-root'] else './models'
 
-    dataLoader= sentence.Sentence(args['--train-src'])
 
     device = torch.device("cuda:0" if args['--cuda'] else "cpu")
     print('use device: %s' % device, file=sys.stderr)
@@ -55,55 +71,21 @@ def train(args: Dict):
         n_gpu = torch.cuda.device_count()
         torch.cuda.get_device_name(0)
 
+
     tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased', do_lower_case=False)
 
-    tokenized_texts = [tokenizer.tokenize(sent) for sent in dataLoader.sentences]
+    labels = get_labels(args['--train-src'] + "/labels.txt")
+    traindataset=loadData(tokenizer,args['--train-src'],'train',MAX_LEN,labels)
+    devdataset = loadData(tokenizer, args['--train-src'], 'dev', MAX_LEN,labels)
 
-    print(dataLoader.sentences[0])
-    print(tokenized_texts[0])
 
-    input_ids = pad_sequences([tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_texts],
-                              maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+    train_sampler = RandomSampler(traindataset)
+    train_dataloader = DataLoader(traindataset, sampler=train_sampler, batch_size=bs)
 
-    tags = pad_sequences([[dataLoader.tag2idx.get(l) for l in lab] for lab in dataLoader.labels],
-                         maxlen=MAX_LEN, value=dataLoader.tag2idx["O"], padding="post",
-                         dtype="long", truncating="post")
+    valid_sampler = SequentialSampler(devdataset)
+    valid_dataloader = DataLoader(devdataset, sampler=valid_sampler, batch_size=bs)
 
-    attention_masks = [[float(i > 0) for i in ii] for ii in input_ids]
-
-    """
-    The BERT Model requires us to have a [SEP] token at the end of each sentence as a part of its preprocessing. 102 is the index BERT recognizes as the index of [SEP]. Hence, I am adding it to the end of the sentence after padding/truncating
-    (as it might have been removed if the sequences were greater than 75 in length) to be compatible with BERT's requirement. I didn't have it in the beginning and I thought it would be the reason for the poor results but changing it didn't help and I chose to keep it anyways as it felt right. :)
-    """
-    for i, inp in enumerate(input_ids):
-        if (102 not in inp):
-            inp[-1] = 102
-            tags[i][-1] = dataLoader.tag2idx.get("O")
-
-    tts = float(args['--train-test-split'])
-
-    tr_inputs, val_inputs, tr_tags, val_tags = train_test_split(input_ids, tags,
-                                                                random_state=10, test_size=tts)
-    tr_masks, val_masks, _, _ = train_test_split(attention_masks, input_ids,
-                                                 random_state=10, test_size=tts)
-
-    tr_inputs = torch.tensor(tr_inputs).to(torch.int64)
-    val_inputs = torch.tensor(val_inputs).to(torch.int64)
-    tr_tags = torch.tensor(tr_tags).to(torch.int64)
-    val_tags = torch.tensor(val_tags).to(torch.int64)
-    tr_masks = torch.tensor(tr_masks)
-    val_masks = torch.tensor(val_masks)
-
-    train_data = TensorDataset(tr_inputs, tr_masks, tr_tags)
-    train_sampler = RandomSampler(train_data)
-    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=bs)
-
-    valid_data = TensorDataset(val_inputs, val_masks, val_tags)
-    valid_sampler = SequentialSampler(valid_data)
-    valid_dataloader = DataLoader(valid_data, sampler=valid_sampler, batch_size=bs)
-
-    model = BertForTokenClassification.from_pretrained(
-        "bert-base-multilingual-cased", num_labels=len(dataLoader.tag2idx))
+    model = BertForTokenClassification.from_pretrained("bert-base-multilingual-cased",num_labels=len(labels))
 
     if args['--cuda']:
         model.cuda()
@@ -136,7 +118,8 @@ def train(args: Dict):
         for step, batch in enumerate(train_dataloader):
             # add batch to gpu
             batch = tuple(t.to(device) for t in batch)
-            b_input_ids, b_input_mask, b_labels = batch
+
+            b_input_ids, b_input_mask,_, b_labels = batch
             # forward pass
             loss = model(b_input_ids, token_type_ids=None,
                          attention_mask=b_input_mask, labels=b_labels)
@@ -160,7 +143,7 @@ def train(args: Dict):
         predictions, true_labels = [], []
         for batch in valid_dataloader:
             batch = tuple(t.to(device) for t in batch)
-            b_input_ids, b_input_mask, b_labels = batch
+            b_input_ids, b_input_mask,_, b_labels = batch
 
             with torch.no_grad():
                 tmp_eval_loss = model(b_input_ids, token_type_ids=None,
@@ -182,8 +165,9 @@ def train(args: Dict):
         eval_loss = eval_loss / nb_eval_steps
         print("Validation loss: {}".format(eval_loss))
         print("Validation Accuracy: {}".format(eval_accuracy / nb_eval_steps))
-        pred_tags = [dataLoader.tags_vals[p_i] for p in predictions for p_i in p]
-        valid_tags = [dataLoader.tags_vals[l_ii] for l in true_labels for l_i in l for l_ii in l_i]
+        label_map = {i: label for i, label in enumerate(labels)}
+        pred_tags = [label_map[p_i] for p in predictions for p_i in p]
+        valid_tags = [label_map[l_ii] for l in true_labels for l_i in l for l_ii in l_i]
         f1=f1_score(valid_tags,pred_tags)
         print("F1-Score: {}".format(f1))
 
@@ -207,7 +191,8 @@ def evaluate(args:Dict):
     model_root = args['--model-root'] if args['--model-root'] else './models'
     print("load model from {}".format(model_root), file=sys.stderr)
 
-    dataLoader = sentence.Sentence(args['--test-src'])
+    labels = get_labels(args['--test-src'] + "/labels.txt")
+
 
     device = torch.device("cuda:0" if args['--cuda'] else "cpu")
 
@@ -215,39 +200,20 @@ def evaluate(args:Dict):
     output_config_file = os.path.join(model_root, "config_file.bin")
     output_vocab_file = os.path.join(model_root, "vocab.txt")
     config = BertConfig.from_json_file(output_config_file)
-    model = BertForTokenClassification(config,num_labels=len(dataLoader.tag2idx))
+    model = BertForTokenClassification(config,num_labels=len(labels))
     state_dict = torch.load(output_model_file)
     model.load_state_dict(state_dict)
     tokenizer = BertTokenizer(output_vocab_file, do_lower_case=False)
 
-    tokenized_texts = [tokenizer.tokenize(sent) for sent in dataLoader.sentences]
 
     if args['--cuda']:
         model = model.to(torch.device("cuda:0"))
 
     MAX_LEN = int(args['--max-len'])
+    testdataset = loadData(tokenizer, args['--test-src'], 'test', MAX_LEN, labels)
 
-    input_ids_test = pad_sequences([tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_texts],
-                              maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
-
-    tags_test = pad_sequences([[dataLoader.tag2idx.get(l) for l in lab] for lab in dataLoader.labels],
-                         maxlen=MAX_LEN, value=dataLoader.tag2idx["O"], padding="post",
-                         dtype="long", truncating="post")
-
-    attention_masks_test = [[float(i > 0) for i in ii] for ii in input_ids_test]
-
-    for i, inp in enumerate(input_ids_test):
-        if (102 not in inp):
-            inp[-1] = 102
-            tags_test[i][-1] = dataLoader.tag2idx.get("O")
-
-    te_inputs = torch.tensor(input_ids_test).to(torch.int64)
-    te_tags = torch.tensor(tags_test).to(torch.int64)
-    te_masks = torch.tensor(attention_masks_test)
-
-    test_data = TensorDataset(te_inputs, te_masks, te_tags)
-    test_sampler = SequentialSampler(test_data)
-    test_dataloader = DataLoader(test_data, sampler=test_sampler, batch_size=int(args['--batch-size']))
+    test_sampler = SequentialSampler(testdataset)
+    test_dataloader = DataLoader(testdataset, sampler=test_sampler, batch_size=int(args['--batch-size']))
 
     model.eval()
     predictions = []
@@ -256,7 +222,7 @@ def evaluate(args:Dict):
     nb_eval_steps, nb_eval_examples = 0, 0
     for batch in test_dataloader:
         batch = tuple(t.to(device) for t in batch)
-        b_input_ids, b_input_mask, b_labels = batch
+        b_input_ids, b_input_mask,_, b_labels = batch
 
         with torch.no_grad():
             tmp_eval_loss = model(b_input_ids, token_type_ids=None,
@@ -276,23 +242,15 @@ def evaluate(args:Dict):
         nb_eval_examples += b_input_ids.size(0)
         nb_eval_steps += 1
 
-    pred_tags = [[dataLoader.tags_vals[p_i] for p_i in p] for p in predictions]
-    test_tags = [[dataLoader.tags_vals[l_ii] for l_ii in l_i] for l in true_labels for l_i in l]
+    label_map = {i: label for i, label in enumerate(labels)}
+    pred_tags = [[label_map[p_i] for p_i in p] for p in predictions]
+    test_tags = [[label_map[l_ii] for l_ii in l_i] for l in true_labels for l_i in l]
 
-    tags_test_fin = list()
-    for l in tags_test:
-        temp_tag = list()
-        for l_i in l:
-            temp_tag.append(dataLoader.tags_vals[l_i])
-        tags_test_fin.append(temp_tag)
 
     print("Test loss: {}".format(eval_loss / nb_eval_steps))
     print("Test Accuracy: {}".format(eval_accuracy / nb_eval_steps))
-    print("Test F1-Score: {}".format(f1_score(tags_test_fin, pred_tags)))
+    print("Test F1-Score: {}".format(f1_score(test_tags, pred_tags)))
 
-    print(classification_report(tags_test_fin, pred_tags))
-
-    print("Number of Test sentences: ", len(tags_test_fin))
 
 def main():
     """ Main func.
